6ed6178df37ae11370cb265989cbc9a424974d70
Reshmi Joseph <reshmijosephk@gmail.com>
2021-11-03 12:05:32 -0400

    added List allTransfers

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -2,6 +2,10 @@ package com.techelevator.tenmo.dao;
 
 import com.techelevator.tenmo.model.Transfer;
 
+import java.util.List;
+
 public interface TransferDao {
-    public Transfer transferLookupWithTransferID(int transferID);
+     List<Transfer> allTransfers(int accountID);
+     Transfer transferLookupWithTransferID(int transferID);
+
 }
