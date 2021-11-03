c91a281154faab1692c148c536731ca1ed0de855
Reshmi Joseph <reshmijosephk@gmail.com>
2021-11-03 12:00:42 -0400

    Made transfer model, dao,jdbc, and controller

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -0,0 +1,13 @@
+package com.techelevator.tenmo.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@PreAuthorize("isAuthenticated()")
+public class TransferController {
+
+
+
+
+}
---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -0,0 +1,7 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Transfer;
+
+public interface TransferDao {
+    public Transfer transferLookupWithTransferID(int transferID);
+}
