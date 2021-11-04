2139eb90b5a6d9ba307e81f69dd80d9209bc2413
Reshmi Joseph <reshmijosephk@gmail.com>
2021-11-04 17:10:43 -0400

    TransferController three methods added

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -1,21 +1,39 @@
 package com.techelevator.tenmo.controller;
 
+import com.techelevator.tenmo.dao.AccountsDao;
 import com.techelevator.tenmo.dao.TransferDAO;
+import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Transfers;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
 @PreAuthorize("iaAuthenticated()")
 public class TransferController {
-    private TransferDAO transferDAO;
+    private TransferDAO transferDao;
+    private AccountsDao accountsDao;
+    private UserDao userDao;
 
-    //don't know if I should op
-    @RequestMapping(path = "account/{id}/transfers", method = RequestMethod.GET)
-    public List<Transfers>listAllTransfers
+
+    //don't know if I should do user or account id
+    @RequestMapping(path = "account/{userID}/transfers", method = RequestMethod.GET)
+    public List<Transfers>listAllTransfersForAccount(@PathVariable Long userID){
+        return transferDao.allTransfers(userID);
+    }
+
+    @RequestMapping(path = "transfers/{transferID}", method = RequestMethod.GET)
+    public Transfers transferLookupWithTransferID(@PathVariable Long transferID){
+        return transferDao.transferLookupWithTransferID(transferID);
+    }
+
+    @RequestMapping(path = "pay" , method = RequestMethod.POST)
+    public Object tenmoPay(@RequestBody Transfers transfer){
+
+        return transferDao.tenmoPay(transfer.getAccountFrom(),transfer.getAccountTo(),transfer.getAmount());
+    }
+//    List<String> userList();
 
 }
